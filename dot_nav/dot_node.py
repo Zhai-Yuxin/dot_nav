333773de6af209e5e30017d393461bd05c8a013b
Zhai Yuxin <e0772744@u.nus.edu>
2025-12-10 07:59:59 +0000

    add cam sub, add task skeleton

--- dot_nav/dot_node.py
+++ dot_nav/dot_node.py
@@ -4,46 +4,79 @@ from nav2_msgs.action import NavigateToPose
 from rclpy.action import ActionClient
 from geometry_msgs.msg import Pose, PoseStamped
 from tf2_msgs.msg import TFMessage
+from sensor_msgs.msg import Image
 
 import math
 import threading
 from time import sleep
+from cv_bridge import CvBridge
+import cv2
 
 class DotNode(Node):
     def __init__(self):
         super().__init__('dot_node')
         self.gt_sub = self.create_subscription(TFMessage, '/ground_truth', self.gt_callback, 10)
-        self.gt = Pose()
+        self.gt = None
+        self.get_gt = False
+
+        self.bridge = CvBridge()
+        self.subscription = self.create_subscription(Image, '/camera/image', self.image_callback, 1)
+        self.view = None
+        self.viewcount = 0
+        self.get_view = False
 
         self.nav_client = ActionClient(self, NavigateToPose, '/navigate_to_pose')
 
+        ##llm setup
+
+        self._stop_event = threading.Event()
         self.task_thread = threading.Thread(target=self.task)
         self.task_thread.start()
 
     def task(self):
-        while rclpy.ok():
+        while rclpy.ok() and not self._stop_event.is_set():
             user_input = input("Instruction: ")
             if user_input.lower() == 'exit':
                 print("ctrl-c to exit")
                 break
+            self.get_view = True
+            self.get_gt = True
+            while self.gt is None or self.view is None:
+                sleep(0.1)
+            cv2.imwrite('/root/Workspaces/camera_images/camera_image_'+str(self.viewcount)+'.png', self.view)
+            self.viewcount += 1
+            print(self.gt)
             sleep(2)
-            ##llm set up + call + output parse
+            ##llm call + output parse
             ##send goal to nav2
             # node.send_nav_goal(-2.0, 1.0, math.radians(180.0))
             # node.send_nav_goal(x, y, math.radians(deg))
+            self.gt = None
+            self.view = None
+
+    def image_callback(self, msg):
+        try:
+            if self.get_view:
+                cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+                self.view = cv_image
+                self.get_view = False
+        except Exception as e:
+            print(f'Failed to process image: {e}')
 
     def gt_callback(self, msg):
         if not msg.transforms:
             return
-
-        for tf in msg.transforms:
-            if tf.child_frame_id == 'simple_car':
-                transform = tf.transform
-                self.gt.position.x = transform.translation.x
-                self.gt.position.y = transform.translation.y
-                self.gt.position.z = transform.translation.z
-                self.gt.orientation = transform.rotation
-                break
+        if self.get_gt:
+            for tf in msg.transforms:
+                if tf.child_frame_id == 'simple_car':
+                    transform = tf.transform
+                    self.gt = Pose()
+                    self.gt.position.x = transform.translation.x
+                    self.gt.position.y = transform.translation.y
+                    self.gt.position.z = transform.translation.z
+                    self.gt.orientation = transform.rotation
+                    break
+            self.get_gt = False
 
     # ros2 action send_goal /navigate_to_pose nav2_msgs/action/NavigateToPose \
     # "{pose: {header: {frame_id: 'map'}, pose: {position: {x: 1.0, y: 2.0, z: 0.0}, orientation: {z: 0.0, w: 1.0}}}}"
@@ -82,12 +115,21 @@ class DotNode(Node):
         print(f'Navigation completed.')
         # print(f'Navigation completed with status: {status}')
 
+    def stop(self):
+        self._stop_event.set()
+        self.task_thread.join()
+
 def main(args=None):
     rclpy.init(args=args)
     node = DotNode()
-    rclpy.spin(node)
-    node.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.stop()
+        node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
